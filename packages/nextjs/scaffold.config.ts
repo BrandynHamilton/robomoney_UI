bc25e10f9dc94588ad8ae4cf9bebe4f1d9673530
BrandynHamilton <brandynham1120@gmail.com>
2025-03-17 10:16:38 -0500

    explicity setting arbitrum as target network

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -1,7 +1,7 @@
-import * as chains from "viem/chains";
+import { type Chain, arbitrum } from "viem/chains";
 
 export type ScaffoldConfig = {
-  targetNetworks: chains.Chain[];
+  targetNetworks: Chain[];
   pollingInterval: number;
   alchemyApiKey: string;
   walletConnectProjectId: string;
@@ -13,7 +13,7 @@ export const DEFAULT_ALCHEMY_API_KEY = "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
 
 const scaffoldConfig = {
   // The networks on which your DApp is live
-  targetNetworks: [chains.arbitrum],
+  targetNetworks: [arbitrum],
 
   // The interval at which your front-end polls the RPC servers for new data
   // it has no effect if you only target the local network (default is 4000)
